a59ce105f8280aa131787906aa8f121e2d6c7c37
MusicXi <792331407@qq.com>
2021-08-13 14:17:52 +0800

    用户管理接口相关优化

--- src/main/java/com/myron/ims/configuration/WebMvcConfig.java
+++ src/main/java/com/myron/ims/configuration/WebMvcConfig.java
@@ -14,5 +14,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        // swagger静态资源访问
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
     }
 }
--- src/main/java/com/myron/ims/controller/UserController.java
+++ src/main/java/com/myron/ims/controller/UserController.java
@@ -1,22 +1,22 @@
 package com.myron.ims.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RequestBody;
 import com.github.pagehelper.Page;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import javax.validation.Valid;
-import com.myron.ims.bean.User;
-import com.myron.ims.service.UserService;
 import com.myron.ims.bean.ApiResult;
 import com.myron.ims.bean.PageResult;
+import com.myron.ims.bean.User;
 import com.myron.ims.exception.BusinessException;
+import com.myron.ims.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
@@ -25,7 +25,7 @@ import com.myron.ims.exception.BusinessException;
  * @date 2020/02/27 16:50:05
  */
 @Api(tags="sys_user 用户信息api接口")
-@Controller
+@RestController
 @RequestMapping("user")
 public class UserController {
 
@@ -33,30 +33,26 @@ public class UserController {
 	private UserService userService;
 
 	@ApiOperation(value="分页查询sys_user 用户信息")
-	@RequestMapping(value = "/list", method=RequestMethod.GET)
-	@ResponseBody
+	@GetMapping(value = "/list")
 	public ApiResult<PageResult<Page<User>>> listByPage(User user, PageResult pageResult) {
 		Page<User> page = this.userService.findListByPage(user, new Page(pageResult.getPageNum(), pageResult.getPageSize()));
 		return ApiResult.wrapPage(page);
 	}
 
 	@ApiOperation(value="添加sys_user 用户信息")
-	@RequestMapping(value = "/add", method=RequestMethod.POST)
-	@ResponseBody
+	@PostMapping(value = "/add")
 	public ApiResult addUser(@RequestBody User user) throws BusinessException {
 		return this.userService.createUser(user);
 	}
 
 	@ApiOperation(value="修改sys_user 用户信息", notes = "#user.username + '的信息被修改了'")
-	@RequestMapping(value = "/edit", method=RequestMethod.POST)
-	@ResponseBody
-	public ApiResult editUser(@RequestBody User user) throws BusinessException {
+	@PutMapping(value = "/edit/{userId}")
+	public ApiResult editUser(@RequestBody User user, @PathVariable("userId") String userId) throws BusinessException {
 		return this.userService.updateUser(user);
 	}
 
 	@ApiOperation(value="删除sys_user 用户信息")
 	@RequestMapping(value = "/delete", method=RequestMethod.POST)
-	@ResponseBody
 	public ApiResult deleteUser(@RequestBody User user) throws BusinessException{
 		return this.userService.deleteUser(user);
 	}
