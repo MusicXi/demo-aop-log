891e5b2c675bc57dec1a4854bd6e48dee7512a4c
MusicXi <792331407@qq.com>
2021-12-02 11:01:27 +0800

    pipeline 执行jenkinsfile报 docker: not found 修复

--- jenkins/scripts/docker-build.sh
+++ jenkins/scripts/docker-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /var/jenkins_home/workspace/create_docker_images/target
 cp ../jenkins/docker/Dockerfile ./
-docker build -t testproject .
+docker build -t linrx1/demo-aop-log:1.0 .
