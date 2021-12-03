4af2a1cdb96a7ba0da116f1c78f3ee2ae71c4831
haha <haha@qq.com>
2021-12-03 15:55:48 +0800

    pipeline parameters 参数测试4

--- jenkins/scripts/docker-build.sh
+++ jenkins/scripts/docker-build.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
-cd /var/jenkins_home/workspace/create_docker_images/target
+
+# 先删除之前的容器
+docker ps -a | grep ${APP_NAME} | awk '{print $1}'| xargs docker rm -f
+# 删除之前的镜像
+docker rmi ${DOCKER_IMAGE_NAME}
+
+# 构建新镜像
+cd ${WORKSPACE}/target
 cp ../jenkins/docker/Dockerfile ./
-docker build -t linrx1/demo-aop-log:1.0 .
+docker build -t ${DOCKER_IMAGE_NAME} .
+
+# 打印当前镜像
+echo "当前docker镜像:docker images | grep ${APP_NAME}"
+docker images | grep ${APP_NAME}
--- jenkins/scripts/docker-run.sh
+++ jenkins/scripts/docker-run.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-docker run -d -p 8088:8088 linrx1/demo-aop-log:1.0
+# 使用当前构建的镜像启动
+docker run -d -p 8088:8088 --name ${APP_NAME} ${DOCKER_IMAGE_NAME}
+
+echo "打印当前容器"
+# 打印当前容器
+docker ps -a | grep  ${DOCKER_IMAGE_REPOSITORY_PREFIX}
